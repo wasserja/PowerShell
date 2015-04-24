02bc843caddba440836caf909f229146956a27a4
wasserja <wasserja@users.noreply.github.com>
2015-04-24 16:11:32 -0400

    4/24/2015 04:11:30 PM

--- Function-Get-StoppedAutomaticServices.ps1
+++ Function-Get-StoppedAutomaticServices.ps1
@@ -14,18 +14,24 @@
    Modified: 4/24/2015 03:24:50 PM 
    Changelog:
     * Cleanup script to output actual service objects.
-
+.PARAMETER ComputerName
+   Enter the name of a computer or a list of computers. Accepts pipeline input. Default: localhost.
+.PARAMETER FilterCleanExit
+   Use this switch if you need to filter out services that stopped cleanly with an exit code 0.
+.PARAMETER ExclusionList
+   Service names that should be excluding from the query. A default list of known services that are set to 
+   Automatic but do not run (i.e. Performance Logs and Alerts) is provided.
 .EXAMPLE
-   .\Get-StoppedAutomaticServices.ps1 -ComputerName server1
+   Get-StoppedAutomaticServices -ComputerName server1
    Lists the stopped automatic services from server1.
 .EXAMPLE
-   .\Get-StoppedAutomaticServices.ps1 -ComputerName server1,server2
+   Get-StoppedAutomaticServices -ComputerName server1,server2
    Lists the stopped automatic services from server1 and server2.
 .EXAMPLE
-   .\Get-StoppedAutomaticServices.ps1 -ComputerName (Get-Content c:\temp\computerlist.txt)
+   Get-StoppedAutomaticServices -ComputerName (Get-Content c:\temp\computerlist.txt)
    Lists the stopped automatic services from a list of computers in a text file.
 .EXAMPLE
-   .\Get-StoppedAutomaticServices.ps1 -FilterCleanExit
+   Get-StoppedAutomaticServices -FilterCleanExit
    Get a list of stopped automatic services that exited cleanly. This should exclude services that do start,
    but have no further work to do (includes Trigger Start)
 #>
@@ -38,15 +44,17 @@ Function Get-StoppedAutomaticServices {
 		    ValueFromPipeLine=$true,
 		    ValueFromPipeLineByPropertyName=$true)]
 	    [string[]]$ComputerName = $env:COMPUTERNAME,
+        
+        # Filter services with exit code 0
         [Parameter(Mandatory=$false,
             ValueFromPipeLine=$false,
 		    ValueFromPipeLineByPropertyName=$true)]
-        [switch]$FilterCleanExit
-    )
-    begin{
-        #Exclusions List - list of known services that are set to Auto but do not run (i.e. Performance Logs and Alerts)
+        [switch]$FilterCleanExit,
+        
+
         $ExclusionList = @('clr_optimization_v4.0.30319_32','clr_optimization_v4.0.30319_64','SysmonLog','ShellHWDetection','sppsvc','gupdate','MMCSS','RemoteRegistry','ccmsetup')
-    }
+    )
+    begin{}
     process {
         foreach ($Computer in $ComputerName) {
             try {
@@ -56,8 +64,9 @@ Function Get-StoppedAutomaticServices {
                 Write-Error "$($_.Exception.Message) $Computer."
                 return
                 }
-            Write-Verbose "Checking services on $computer using exclusion list:"
-            $ExclusionList | ForEach-Object {Write-Verbose $_}
+            Write-Verbose "Checking services on $computer"
+            write-Verbose "Exclusion List:"
+            $ExclusionList | ForEach-Object {Write-Verbose " * $_"}
             if ($FilterCleanExit) {
                 $stoppedautoservices = Get-WmiObject win32_service -computername $computer -filter "state = 'stopped' and startmode = 'auto' and exitcode != 0" | 
                 Where-Object { $ExclusionList -notcontains $_.name }
@@ -68,26 +77,16 @@ Function Get-StoppedAutomaticServices {
                 }
             if ( $stoppedautoservices ) {
                 Write-Verbose "Services needing attention:"
-                $stoppedautoservices | ForEach-Object {Write-Verbose $_.DisplayName}
-                #Write-Verbose $($stoppedautoservices | Select-Object -Property PSComputerName,Name,DisplayName,startmode,state,exitcode | Format-Table –autosize)
-                $stoppedautoservices
-                Write-Verbose "To start the service type: "
-                Write-Verbose "Invoke-Command -computername servername {start-service nameofservice}"
-                if ( $stoppedautoservices.count ) {
-                    foreach ($stoppedautoservice in $stoppedautoservices) {
-                        Write-Verbose "Example: Invoke-Command -computername $computer {start-service $($stoppedautoservice.name)}"
-                        }
-                    }
-                else {
-                    $examplecomputer = $stoppedautoservices.__SERVER
-                    $exampleservice = $stoppedautoservices.name
-                    Write-Verbose "Example: Invoke-Command -computername $examplecomputer {start-service $exampleservice}"
-                    }
+                $stoppedautoservices | ForEach-Object {Write-Verbose " * $($_.DisplayName)"}
+                $stoppedautoservicesobject = @()
+                $stoppedautoservicesobject += Get-Service $stoppedautoservices.name -ComputerName $stoppedautoservices.PSComputerName
+                $stoppedautoservicesobject
+                # 
                 }
             else {
                 Write-Verbose "$Computer`: All services ok."
                 }
-        }
+            }
         }
     end {}
 }
