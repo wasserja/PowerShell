43b87f6d65af6f6f80e03b8c1760def333a94c22
wasserja <wasserja@users.noreply.github.com>
2015-04-24 16:28:35 -0400

    4/24/2015 04:28:35 PM

--- Function-Get-StoppedAutomaticServices.ps1
+++ Function-Get-StoppedAutomaticServices.ps1
@@ -4,11 +4,6 @@
 .DESCRIPTION
    The script makes a WMI query for all services that have an Automatic start mode, but are stopped. 
    This is useful for checking servers after patching for any services that didn't start automatically.
-
-   The $ExclusionList is an array that should be added to for any service you wish to ignore.
-
-   $FilterCleanExit can be used to automatically exclude services which start but then exit 
-   because they have nothing else to do (or are trigger start).
 .NOTES
    By: Jason Wasser
    Modified: 4/24/2015 03:24:50 PM 
@@ -17,25 +12,29 @@
 .PARAMETER ComputerName
    Enter the name of a computer or a list of computers. Accepts pipeline input. Default: localhost.
 .PARAMETER FilterCleanExit
-   Use this switch if you need to filter out services that stopped cleanly with an exit code 0.
+   Use to automatically exclude services which start but then exit because they have nothing else 
+   to do (or are trigger start). Use this switch if you need to filter out services that stopped 
+   cleanly with an exit code 0. 
 .PARAMETER ExclusionList
    Service names that should be excluding from the query. A default list of known services that are set to 
    Automatic but do not run (i.e. Performance Logs and Alerts) is provided.
 .EXAMPLE
-   Get-StoppedAutomaticServices -ComputerName server1
+   Get-StoppedAutomaticService -ComputerName server1
    Lists the stopped automatic services from server1.
 .EXAMPLE
-   Get-StoppedAutomaticServices -ComputerName server1,server2
+   Get-StoppedAutomaticService -ComputerName server1,server2
    Lists the stopped automatic services from server1 and server2.
 .EXAMPLE
-   Get-StoppedAutomaticServices -ComputerName (Get-Content c:\temp\computerlist.txt)
+   Get-StoppedAutomaticService -ComputerName (Get-Content c:\temp\computerlist.txt)
    Lists the stopped automatic services from a list of computers in a text file.
 .EXAMPLE
-   Get-StoppedAutomaticServices -FilterCleanExit
+   Get-StoppedAutomaticService -FilterCleanExit
    Get a list of stopped automatic services that exited cleanly. This should exclude services that do start,
    but have no further work to do (includes Trigger Start)
+.EXAMPLE
+   Get-Content C:\Temp\serverlist.txt | Get-StoppedAutomaticService | Start-Service
 #>
-Function Get-StoppedAutomaticServices {
+Function Get-StoppedAutomaticService {
     [CmdletBinding()]
     [OutputType([System.ServiceProcess.ServiceController])]
     param
@@ -68,19 +67,19 @@ Function Get-StoppedAutomaticServices {
             write-Verbose "Exclusion List:"
             $ExclusionList | ForEach-Object {Write-Verbose " * $_"}
             if ($FilterCleanExit) {
-                $stoppedautoservices = Get-WmiObject win32_service -computername $computer -filter "state = 'stopped' and startmode = 'auto' and exitcode != 0" | 
+                $StoppedAutomaticService = Get-WmiObject -Class win32_service -ComputerName $computer -Filter "state = 'stopped' and startmode = 'auto' and exitcode != 0" | 
                 Where-Object { $ExclusionList -notcontains $_.name }
                 }
             else {
-                $stoppedautoservices = Get-WmiObject win32_service -computername $computer -filter "state = 'stopped' and startmode = 'auto'" | 
+                $StoppedAutomaticService = Get-WmiObject -Class win32_service -ComputerName $computer -Filter "state = 'stopped' and startmode = 'auto'" | 
                 Where-Object { $ExclusionList -notcontains $_.name }
                 }
-            if ( $stoppedautoservices ) {
+            if ( $StoppedAutomaticService ) {
                 Write-Verbose "Services needing attention:"
-                $stoppedautoservices | ForEach-Object {Write-Verbose " * $($_.DisplayName)"}
-                $stoppedautoservicesobject = @()
-                $stoppedautoservicesobject += Get-Service $stoppedautoservices.name -ComputerName $stoppedautoservices.PSComputerName
-                $stoppedautoservicesobject
+                $StoppedAutomaticService | ForEach-Object {Write-Verbose " * $($_.DisplayName)"}
+                $StoppedAutomaticServiceObject = @()
+                $StoppedAutomaticServiceObject += Get-Service $StoppedAutomaticService.name -ComputerName $StoppedAutomaticService.PSComputerName
+                $StoppedAutomaticServiceObject
                 # 
                 }
             else {
