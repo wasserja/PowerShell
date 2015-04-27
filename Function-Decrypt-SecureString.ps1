a6ab5228c2d0e15014835823c4739e4d6080b5c7
wasserja <wasserja@users.noreply.github.com>
2015-04-27 11:19:26 -0400

    4/27/2015 11:19:23 AM

---
+++ Function-Decrypt-SecureString.ps1
@@ -0,0 +1,17 @@
+﻿Function Decrypt-SecureString { 
+    param( 
+        [Parameter(ValueFromPipeline=$true,
+                    Mandatory=$true,
+                    Position=0)] 
+        [System.Security.SecureString]$SecureString 
+    ) 
+    begin {} 
+    process { 
+        $marshal = [System.Runtime.InteropServices.Marshal] 
+        $ptr = $marshal::SecureStringToBSTR( $SecureString ) 
+        $DecryptedString = $marshal::PtrToStringBSTR( $ptr ) 
+        $marshal::ZeroFreeBSTR( $ptr ) 
+        $DecryptedString 
+        } 
+    end {} 
+    } 
