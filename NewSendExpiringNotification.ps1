e4864268409cfe1299d95e4d60a9c04cf7a28c57
wasserja <wasserja@users.noreply.github.com>
2015-05-12 08:58:03 -0400

    5/12/2015 08:58:01 AM

--- NewSendExpiringNotification.ps1
+++ NewSendExpiringNotification.ps1
@@ -1,12 +1,12 @@
 ﻿<#
 .Synopsis
-   Short description
+   Send-PasswordExpirationNotification
 .DESCRIPTION
-   Long description
+   Send-PasswordExpirationNotification
 .EXAMPLE
-   Example of how to use this cmdlet
+   Send-PasswordExpirationNotification
 .EXAMPLE
-   Another example of how to use this cmdlet
+   Send-PasswordExpirationNotification
 #>
 function Send-PasswordExpirationNotification
 {
@@ -14,11 +14,11 @@ function Send-PasswordExpirationNotification
     [Alias()]
     Param
     (
-        # Param1 help description
-        [Parameter(Mandatory=$true,
+        # Username
+        [Parameter(Mandatory=$false,
                    ValueFromPipelineByPropertyName=$true,
                    Position=0)]
-        $Param1,
+        [string[]]$Username='*',
 
         # How many days prior to expiration
         [int]$NotificationStartDay = 10,
@@ -34,117 +34,122 @@ function Send-PasswordExpirationNotification
 
     Begin
     {
+        ##############Variables#################                    
+        # SMTP Authentication
+        $SecurePassword = ConvertTo-SecureString -String $SMTPPassword -AsPlainText -Force
+        $Credential = New-Object System.Management.Automation.PSCredential($SMTPUsername,$SecurePassword)
+        ######################################## 
+
+        ##############Function##################            
+        function PreparePasswordPolicyMail ($ComplexityEnabled,$MaxPasswordAge,$MinPasswordAge,$MinPasswordLength,$PasswordHistoryCount)            
+        {            
+            $verbosemailBody = "Below is a summary of the applied Password Policy settings:`r`n`r`n"            
+            $verbosemailBody += "Complexity Enabled = " + $ComplexityEnabled + "`r`n`r`n"            
+            $verbosemailBody += "Maximum Password Age = " + $MaxPasswordAge + "`r`n`r`n"            
+            $verbosemailBody += "Minimum Password Age = " + $MinPasswordAge + "`r`n`r`n"            
+            $verbosemailBody += "Minimum Password Length = " + $MinPasswordLength + "`r`n`r`n"            
+            $verbosemailBody += "Remembered Password History = " + $PasswordHistoryCount + "`r`n`r`n"            
+            return $verbosemailBody            
+        }            
+            
+        function SendMail ($SMTPserver,$sendermailaddress,$usermailaddress,$mailBody)            
+        {            
+            $smtpServer = $SMTPserver            
+            $msg = new-object Net.Mail.MailMessage            
+            $smtp = new-object Net.Mail.SmtpClient($smtpServer)            
+            $msg.From = $sendermailaddress            
+            $msg.To.Add($usermailaddress)            
+            $msg.Subject = "Your password is about to expire"            
+            $msg.Body = $mailBody            
+            $smtp.Send($msg)            
+        }            
+        ########################################            
+        ##############Main######################            
+        $domainPolicy = Get-ADDefaultDomainPasswordPolicy            
+        $passwordexpirydefaultdomainpolicy = $domainPolicy.MaxPasswordAge.Days -ne 0            
+            
+        if($passwordexpirydefaultdomainpolicy)            
+        {            
+            $defaultdomainpolicyMaxPasswordAge = $domainPolicy.MaxPasswordAge.Days            
+            if($verbose)            
+            {            
+                $defaultdomainpolicyverbosemailBody = PreparePasswordPolicyMail $PSOpolicy.ComplexityEnabled $PSOpolicy.MaxPasswordAge.Days $PSOpolicy.MinPasswordAge.Days $PSOpolicy.MinPasswordLength $PSOpolicy.PasswordHistoryCount            
+            }            
+        }            
+
     }
     Process
     {
+        # foreach ($user in (Get-ADUser -SearchBase "DC=welldynerx,DC=com" -Filter '*' -properties mail)) 
+        foreach ($user in (Get-ADUser -SearchBase "DC=domain,DC=com" -Filter '' -properties mail | ? {$_.samaccountname -match "bvazquez|jwasser"}  ))
+        {           
+         $samaccountname = $user.samaccountname            
+            $PSO= Get-ADUserResultantPasswordPolicy -Identity $samaccountname            
+            if ($PSO -ne $null)            
+            {                         
+                $PSOpolicy = Get-ADUserResultantPasswordPolicy -Identity $samaccountname            
+                $PSOMaxPasswordAge = $PSOpolicy.MaxPasswordAge.days            
+                $pwdlastset = [datetime]::FromFileTime((Get-ADUser -LDAPFilter "(&(samaccountname=$samaccountname))" -properties pwdLastSet).pwdLastSet)            
+                $expirydate = ($pwdlastset).AddDays($PSOMaxPasswordAge)            
+                $delta = ($expirydate - (Get-Date)).Days            
+                $comparionresults = (($expirydate - (Get-Date)).Days -le $notificationstartday) -AND ($delta -ge 1)            
+                if ($comparionresults)            
+                {            
+                    $mailBody = "Dear " + $user.GivenName + ",`r`n`r`n"            
+                    $mailBody += "Your Windows password will expire after " + $delta + " days. You will need to change your password to keep using it.`r`n`r`n"            
+                    if ($verbose)            
+                    {            
+                        $mailBody += PreparePasswordPolicyMail $PSOpolicy.ComplexityEnabled $PSOpolicy.MaxPasswordAge.Days $PSOpolicy.MinPasswordAge.Days $PSOpolicy.MinPasswordLength $PSOpolicy.PasswordHistoryCount            
+                    }            
+                    $mailBody += "`r`n`r`nWellDyneRx IT Department"            
+                    $usermailaddress = $user.mail            
+                    SendMail $SMTPserver $sendermailaddress $usermailaddress $mailBody            
+                }            
+            }            
+            else            
+            {            
+                if($passwordexpirydefaultdomainpolicy)            
+                {            
+                    $pwdlastset = [datetime]::FromFileTime((Get-ADUser -LDAPFilter "(&(samaccountname=$samaccountname))" -properties pwdLastSet).pwdLastSet)            
+                    $expirydate = ($pwdlastset).AddDays($defaultdomainpolicyMaxPasswordAge)            
+                    $delta = ($expirydate - (Get-Date)).Days            
+                    $comparionresults = (($expirydate - (Get-Date)).Days -le $notificationstartday) -AND ($delta -ge 1)            
+                    if ($comparionresults)            
+                    {            
+                        $expiringpassusers += $samaccountname
+                        $mailBody = "Dear " + $user.GivenName + ",`r`n`r`n"            
+                        $delta = ($expirydate - (Get-Date)).Days            
+                        $mailBody += "Your Windows password will expire after " + $delta + " days. You will need to change your password to keep using your account. You will continue to receive a daily reminder until you do change it. `r`n`r`n"            
+                        $mailBody += "If you need any help resetting your password please contact our helpdesk at 855-404-0966 or internally at ext. 5555. `r`n`r`n" 
+                        if ($verbose)            
+                        {            
+                            $mailBody += $defaultdomainpolicyverbosemailBody            
+                        }            
+                        $mailBody += "`r`n`r`n IT Department"            
+                        $usermailaddress = $user.mail            
+                        SendMail $SMTPserver $sendermailaddress $usermailaddress $mailBody
+                           
+                    }            
+            
+                }            
+            }            
+            }
     }
     End
     {
+        #Sending list to Help Desk 
+        #SendMail $SMTPserver $sendermailaddress $usermailaddress $mailBody
+        $expiringpassusers | foreach {$messagebody = $messagebody + "`n" + $_} 
+        Send-MailMessage -to email@domain.com -From automaton@domain.com -Subject "List of Users with Expiring Password" -Body $messagebody -SmtpServer $SMTPserver -Credential $credential 
     }
 }
 
 
-##############Variables#################                    
-# SMTP Authentication
-$SecurePassword = ConvertTo-SecureString -String $SMTPPassword -AsPlainText -Force
-$Credential = New-Object System.Management.Automation.PSCredential($SMTPUsername,$SecurePassword)
-########################################            
+           
             
-##############Function##################            
-function PreparePasswordPolicyMail ($ComplexityEnabled,$MaxPasswordAge,$MinPasswordAge,$MinPasswordLength,$PasswordHistoryCount)            
-{            
-    $verbosemailBody = "Below is a summary of the applied Password Policy settings:`r`n`r`n"            
-    $verbosemailBody += "Complexity Enabled = " + $ComplexityEnabled + "`r`n`r`n"            
-    $verbosemailBody += "Maximum Password Age = " + $MaxPasswordAge + "`r`n`r`n"            
-    $verbosemailBody += "Minimum Password Age = " + $MinPasswordAge + "`r`n`r`n"            
-    $verbosemailBody += "Minimum Password Length = " + $MinPasswordLength + "`r`n`r`n"            
-    $verbosemailBody += "Remembered Password History = " + $PasswordHistoryCount + "`r`n`r`n"            
-    return $verbosemailBody            
-}            
             
-function SendMail ($SMTPserver,$sendermailaddress,$usermailaddress,$mailBody)            
-{            
-    $smtpServer = $SMTPserver            
-    $msg = new-object Net.Mail.MailMessage            
-    $smtp = new-object Net.Mail.SmtpClient($smtpServer)            
-    $msg.From = $sendermailaddress            
-    $msg.To.Add($usermailaddress)            
-    $msg.Subject = "Your password is about to expire"            
-    $msg.Body = $mailBody            
-    $smtp.Send($msg)            
-}            
-########################################            
+
             
-##############Main######################            
-$domainPolicy = Get-ADDefaultDomainPasswordPolicy            
-$passwordexpirydefaultdomainpolicy = $domainPolicy.MaxPasswordAge.Days -ne 0            
-            
-if($passwordexpirydefaultdomainpolicy)            
-{            
-    $defaultdomainpolicyMaxPasswordAge = $domainPolicy.MaxPasswordAge.Days            
-    if($verbose)            
-    {            
-        $defaultdomainpolicyverbosemailBody = PreparePasswordPolicyMail $PSOpolicy.ComplexityEnabled $PSOpolicy.MaxPasswordAge.Days $PSOpolicy.MinPasswordAge.Days $PSOpolicy.MinPasswordLength $PSOpolicy.PasswordHistoryCount            
-    }            
-}            
-            
-# foreach ($user in (Get-ADUser -SearchBase "DC=welldynerx,DC=com" -Filter '*' -properties mail)) 
-  foreach ($user in (Get-ADUser -SearchBase "DC=welldynerx,DC=com" -Filter '*' -properties mail | ? {$_.samaccountname -match "bvazquez|jwasser"}  ))
 
 
-{           
- $samaccountname = $user.samaccountname            
-    $PSO= Get-ADUserResultantPasswordPolicy -Identity $samaccountname            
-    if ($PSO -ne $null)            
-    {                         
-        $PSOpolicy = Get-ADUserResultantPasswordPolicy -Identity $samaccountname            
-        $PSOMaxPasswordAge = $PSOpolicy.MaxPasswordAge.days            
-        $pwdlastset = [datetime]::FromFileTime((Get-ADUser -LDAPFilter "(&(samaccountname=$samaccountname))" -properties pwdLastSet).pwdLastSet)            
-        $expirydate = ($pwdlastset).AddDays($PSOMaxPasswordAge)            
-        $delta = ($expirydate - (Get-Date)).Days            
-        $comparionresults = (($expirydate - (Get-Date)).Days -le $notificationstartday) -AND ($delta -ge 1)            
-        if ($comparionresults)            
-        {            
-            $mailBody = "Dear " + $user.GivenName + ",`r`n`r`n"            
-            $mailBody += "Your Windows password will expire after " + $delta + " days. You will need to change your password to keep using it.`r`n`r`n"            
-            if ($verbose)            
-            {            
-                $mailBody += PreparePasswordPolicyMail $PSOpolicy.ComplexityEnabled $PSOpolicy.MaxPasswordAge.Days $PSOpolicy.MinPasswordAge.Days $PSOpolicy.MinPasswordLength $PSOpolicy.PasswordHistoryCount            
-            }            
-            $mailBody += "`r`n`r`nWellDyneRx IT Department"            
-            $usermailaddress = $user.mail            
-            SendMail $SMTPserver $sendermailaddress $usermailaddress $mailBody            
-        }            
-    }            
-    else            
-    {            
-        if($passwordexpirydefaultdomainpolicy)            
-        {            
-            $pwdlastset = [datetime]::FromFileTime((Get-ADUser -LDAPFilter "(&(samaccountname=$samaccountname))" -properties pwdLastSet).pwdLastSet)            
-            $expirydate = ($pwdlastset).AddDays($defaultdomainpolicyMaxPasswordAge)            
-            $delta = ($expirydate - (Get-Date)).Days            
-            $comparionresults = (($expirydate - (Get-Date)).Days -le $notificationstartday) -AND ($delta -ge 1)            
-            if ($comparionresults)            
-            {            
-                $expiringpassusers += $samaccountname
-                $mailBody = "Dear " + $user.GivenName + ",`r`n`r`n"            
-                $delta = ($expirydate - (Get-Date)).Days            
-                $mailBody += "Your Windows password will expire after " + $delta + " days. You will need to change your password to keep using your account. You will continue to receive a daily reminder until you do change it. `r`n`r`n"            
-                $mailBody += "If you need any help resetting your password please contact our helpdesk at 855-404-0966 or internally at ext. 5555. `r`n`r`n" 
-                if ($verbose)            
-                {            
-                    $mailBody += $defaultdomainpolicyverbosemailBody            
-                }            
-                $mailBody += "`r`n`r`n IT Department"            
-                $usermailaddress = $user.mail            
-                SendMail $SMTPserver $sendermailaddress $usermailaddress $mailBody
-                           
-            }            
-            
-        }            
-    }            
-}
-#Sending list to Help Desk 
-#SendMail $SMTPserver $sendermailaddress $usermailaddress $mailBody
-$expiringpassusers | foreach {$messagebody = $messagebody + "`n" + $_} 
-Send-MailMessage -to email@domain.com -From automaton@domain.com -Subject "List of Users with Expiring Password" -Body $messagebody -SmtpServer $SMTPserver -Credential $credential 
+
