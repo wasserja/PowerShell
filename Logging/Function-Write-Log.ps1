e99b244688de653a569e48130089ce27f17a15d6
wasserja <wasserja@users.noreply.github.com>
2015-11-22 15:13:31 -0500

    Cosmetic updates to Write-Log

--- Logging/Function-Write-Log.ps1
+++ Logging/Function-Write-Log.ps1
@@ -5,9 +5,6 @@
    The Write-Log function is designed to add logging capability to other scripts.
    In addition to writing output and/or verbose you can write to a log file for
    later debugging.
-
-   By default the function will create the path and file if it does not 
-   exist. 
 .NOTES
    Created by: Jason Wasser @wasserja
    Modified: 11/19/2015 03:44:50 PM 
@@ -22,21 +19,22 @@
     * Add ability to write $Message to $Verbose or $Error pipelines to eliminate
       duplicates.
 .PARAMETER Message
-   Message is the content that you wish to add to the log file.
+   Message is the content that you wish to add to the log file. 
 .PARAMETER Path
-   The path to the log file to which you would like to write.
+   The path to the log file to which you would like to write. By default the function will 
+   create the path and file if it does not exist. 
 .PARAMETER Level
    Specify the criticality of the log information being written to the log (i.e. Error, Warning, Informational)
 .PARAMETER NoClobber
    Use NoClobber if you do not wish to overwrite an existing file.
 .EXAMPLE
-   Write-Log -Message "Log message" 
-   Writes the message to c:\Logs\PowerShellLog.log
+   Write-Log -Message 'Log message' 
+   Writes the message to c:\Logs\PowerShellLog.log.
 .EXAMPLE
-   Write-Log -Message "Restarting Server" -Path c:\Logs\Scriptoutput.log
+   Write-Log -Message 'Restarting Server.' -Path c:\Logs\Scriptoutput.log
    Writes the content to the specified log file and creates the path and file specified. 
 .EXAMPLE
-   Write-Log -Message "Does not exist" -Path c:\Logs\Script.log -Level Error
+   Write-Log -Message 'Folder does not exist.' -Path c:\Logs\Script.log -Level Error
    Writes the message to the specified log file as an error message, and writes the message to the error pipeline.
 .LINK
    https://gallery.technet.microsoft.com/scriptcenter/Write-Log-PowerShell-999c32d0
@@ -46,7 +44,6 @@ function Write-Log
     [CmdletBinding()]
     Param
     (
-        # The string to be written to the log.
         [Parameter(Mandatory=$true,
                    ValueFromPipelineByPropertyName=$true,
                    Position=0)]
@@ -54,39 +51,37 @@ function Write-Log
         [Alias("LogContent")]
         [string]$Message,
 
-        # The path to the log file.
         [Parameter(Mandatory=$false,
                    ValueFromPipelineByPropertyName=$true,
                    Position=1)]
         [Alias('LogPath')]
         [string]$Path='C:\Logs\PowerShellLog.log',
         
-        # Level of severity for the log message.
         [Parameter(Mandatory=$false,
                     ValueFromPipelineByPropertyName=$true,
                     Position=3)]
         [ValidateSet("Error","Warn","Info")]
         [string]$Level="Info",
         
-        # Do not overwrite existing file.
         [Parameter(Mandatory=$false)]
         [switch]$NoClobber
     )
 
     Begin
     {
+        # Set VerbosePreference to Continue so that verbose messages are displayed.
         $VerbosePreference = 'Continue'
     }
     Process
     {
         
+        # If the file already exists and NoClobber was specified, do not write to the log.
         if ((Test-Path $Path) -AND $NoClobber) {
             Write-Warning "Log file $Path already exists, and you specified NoClobber. Either delete the file or specify a different name."
             Return
             }
 
-        # If attempting to write to a log file in a folder/path that doesn't exist
-        # to create the file include path.
+        # If attempting to write to a log file in a folder/path that doesn't exist create the file including the path.
         elseif (!(Test-Path $Path)) {
             Write-Verbose "Creating $Path."
             $NewLogFile = New-Item $Path -Force -ItemType File
@@ -96,7 +91,7 @@ function Write-Log
             # Nothing to see here yet.
             }
 
-        # Now do the logging and additional output based on $Level
+        # Now do the logging and additional output based on $Level.
         switch ($Level) {
             'Error' {
                 Write-Error $Message
