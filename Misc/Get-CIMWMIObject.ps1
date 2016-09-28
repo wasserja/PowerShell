62ad692d1aad84b8ead81a8649432b67377faed6
wasserja <wasserja@users.noreply.github.com>
2016-09-28 16:59:15 -0400

    added get-cimwmiobject

---
+++ Misc/Get-CIMWMIObject.ps1
@@ -0,0 +1,15 @@
+﻿param (
+    [string]$ClassName = 'win32_operatingsystem',
+    [string[]]$ComputerName=$env:COMPUTERNAME,
+    $Namespace='root\cimv2'
+    )
+
+# Try CIM first, fall back on WMI
+
+try {
+    $isWSManAlive = if (Test-WSMan -ComputerName) {
+        $Data = Get-CimInstance -Namespace $Namespace -ClassName $ClassName -ComputerName $ComputerName
+        } 
+    }
+catch {
+    }
