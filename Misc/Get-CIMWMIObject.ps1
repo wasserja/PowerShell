b0e64e943c9acef43fdc599ff116c4abee67a99f
wasserja <wasserja@users.noreply.github.com>
2016-09-29 14:15:46 -0400

    small progress

--- Misc/Get-CIMWMIObject.ps1
+++ Misc/Get-CIMWMIObject.ps1
@@ -1,25 +1,29 @@
-﻿param (
-    [string]$ClassName = 'win32_operatingsystem',
+﻿[CmdletBinding()]
+param (
+    [string]$Class = 'win32_operatingsystem',
     [string[]]$ComputerName=$env:COMPUTERNAME,
-    $Namespace='root\cimv2'
+    [string]$Namespace='root\cimv2'
     )
 
 # Try CIM first, fall back on WMI
-
+begin {}
 process {
     foreach ($Computer in $ComputerName) {
         
        try {
-            if (Test-WSMan -ComputerName $Computer) {
-            $Data = Get-CimInstance -Namespace $Namespace -ClassName $ClassName -ComputerName $Computer
-            
-            } 
+            $Data = Get-CimInstance -Namespace $Namespace -ClassName $Class -ComputerName $Computer -ErrorAction Stop 
         }
         catch {
-            Write-Error $Error[0].Exception
+            Write-Verbose "Unable to connect to $Computer using WSMan protocol. $($Error[0].Exception.Message) Falling back to WMI."
+            try {
+                $Data = Get-WmiObject -Namespace $Namespace -Class $Class -ComputerName $Computer -ErrorAction Stop
+                }
+            catch {
+                Write-Error "Unable to connect to $Computer using CIM or WMI $($Error[0].Exception)"
+                return
+                }
             } 
-
         $Data
         }
-    
     }
+end {}
