788256a34b1e7702e0bc6816705e59b4f99b707b
@wasserja <wasserja@users.noreply.github.com>
2017-07-18 01:17:20 +0000

    File logger v1

---
+++ Tanium/Get-FileList.ps1
@@ -0,0 +1,32 @@
+[CmdletBinding()]
+param (
+    [ValidateScript( {Test-Path -Path $_ -PathType Container})]    
+    [string]$Folder = 'C:\Windows\system32\drivers\etc',
+    [bool]$Recurse = $true,
+    [ValidateScript( {Test-Path -Path $_ -PathType Container})]
+    [string]$LogPath = 'C:\Logs'
+        
+)
+    
+begin {
+    $FileListLog = $LogPath + '\FileListLog-' + "$(Get-Date -Format 'yyyyMMddHHmmss')" + '.log'
+}
+    
+process {
+
+    
+    if ($Recurse) {
+        Write-Verbose -Message "Recursively logging all files from $Folder to $FileListLog"
+        Get-ChildItem -Path $Folder -Recurse | Select-Object -ExpandProperty FullName | Out-File -FilePath $FileListLog
+    }
+    else {
+        Write-Verbose -Message "Logging all files from $Folder to $FileListLog"
+        Get-ChildItem -Path $Folder | Select-Object -ExpandProperty FullName | Out-File -FilePath $FileListLog
+    }
+    Start-Process -FilePath $FileListLog
+
+}
+    
+end {
+}
+
